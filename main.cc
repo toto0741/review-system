1853d357dbf18ba715e5635ebc41962d859a91b8
nke94 <nke94@samsung.com>
2018-08-14 17:07:44 +0900

    implement add function

--- main.cc
+++ main.cc
@@ -2,6 +2,14 @@
 
 #include <stdio.h>
 
+int add(int a, int b)
+{
+	//check return code
+	if(a<0) return -1;
+	if(b<0) return -1;
+	return a+b;
+}
+
 int main(){
 	printf("hello world!!!\n");
 	return 0;
